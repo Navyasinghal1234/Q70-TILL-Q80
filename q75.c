fe51dcbc47e0cbd41d33d16de52abdc3bb56ea1c
Navyasinghal1234 <write24navya@gmail.com>
2025-11-18 01:19:23 +0530

    q75.c

---
+++ q75.c
@@ -0,0 +1,42 @@
+//WAP TO ADD TWO MATRICES.
+#include <stdio.h>
+
+int main() {
+    int r, c;
+
+    // Reading number of rows and columns
+    scanf("%d %d", &r, &c);
+
+    int a[r][c], b[r][c], sum[r][c];
+
+    // Reading first matrix
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Reading second matrix
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Adding both matrices
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    // Printing result matrix
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
